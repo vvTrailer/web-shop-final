fa9e6bb58ee169126084dbf4d4a3648315eb4908
Filip Dusek <fidu@html24.net>
2017-05-11 19:31:51 +0200

    Main page sidebar + product cards

---
+++ public/app/app.js
@@ -0,0 +1,10 @@
+var app = angular.module('webshopApp', ['ngRoute', 'ngResource']).run(function($rootScope) {
+	$rootScope.authenticated = false;
+	$rootScope.current_user = '';
+	
+	$rootScope.signout = function(){
+    	$http.get('auth/signout');
+    	$rootScope.authenticated = false;
+    	$rootScope.current_user = '';
+	};
+});
