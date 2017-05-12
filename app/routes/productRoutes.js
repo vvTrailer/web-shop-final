3208546771aaedbc4d02770a888010ecf7e4ac25
Filip Dusek <fidu@html24.net>
2017-05-12 16:10:11 +0200

    added product remove route

--- app/routes/productRoutes.js
+++ app/routes/productRoutes.js
@@ -2,30 +2,43 @@ var Product       = require('../models/product');
 
 module.exports = function(app, express) {
 	var apiRouter = express.Router();
+
 	apiRouter.route('/products').post(function(req, res) {
+		//TODO: check if admin
 		var product = new Product();
 		product.name = req.body.name;
 		product.description = req.body.description;
 		product.image = req.body.image; // This is just a link for now
 		product.price = req.body.price;
 
-		product.save(function(err) {
+		product.save(function(err, doc) {
 			if (err) {
 				return res.send(err);
 			}
-
-			// return a message
-			res.json({ message: 'Product created!' });
+			res.json({ message: 'Product created!', object: doc });
 		});
 	})		
 	.get(function(req, res) {
 		Product.find(function(err, products) {
-			if (err) res.send(err);
-
-			// return the users
+			if (err) {
+				res.send(err);
+			}
 			res.json(products);
 		});
 	});
+
+	apiRouter.route('/products/:id').delete(function(req, res){
+		//TODO: check if admin
+		Product.remove({_id: req.params.id}, function(err,removed) {
+			if (err) {
+				return res.send(err);
+			} 
+			if (removed.result.n < 1){
+				return res.status(404).json({ message: 'No such product found!' });
+			}
+			res.json({ message: 'Product deleted!' })
+		});
+	})
 
 	return apiRouter;
 }
